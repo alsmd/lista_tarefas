4d7f153bbc872ea446acb19b3eae3a29b7bcab4f
Celebro01 <dangerveneta@gmail.com>
2020-09-26 17:12:33 -0300

    comentario

--- public/php/registrar_tarefa.php
+++ public/php/registrar_tarefa.php
@@ -4,6 +4,8 @@
     foreach($_POST as $i => $dado){
         $_POST[$i] = str_replace("#","-",$_POST[$i]);
     }
+
+    //recupera o id da conta logada no momento e armazena a tarefa no arquivo correspondente ao id
     $id =$_SESSION['id'];
     $tarefa = implode("#",$_POST).PHP_EOL;
     echo $tarefa;
